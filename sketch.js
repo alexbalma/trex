4fe9d5cda553386f1c97b60ab20bee95c520b2ce
alexbalma <99622713+alexbalma@users.noreply.github.com>
2022-02-14 07:15:48 -0600

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,25 +1,44 @@
-var box;
 
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(400,400);
+  background(51);
   box = createSprite(200,200,30,30);
+
 }
 
-function draw() {
-  background(30);
+function draw() 
+{
 
-  if (keyDown("RIGHT_ARROW")) {
-    box.position.x = box.position.x + 5;
-  }
-  if (keyDown("LEFT_ARROW")) {
-    box.position.x = box.position.x - 5;
-  }
-  if (keyDown("UP_ARROW")) {
-    box.position.y = box.position.y - 5;
-  }
-  if (keyDown("DOWN_ARROW")) {
-    box.position.y = box.position.y + 5;
+  // escribir el código para cambiar el color del fondo \
+  background("yellow");
+  // rojo cuando se presiona RIGHT_ARROW (tecla de flecha derecha)
+  if (keyIsDown(RIGHT_ARROW)) 
+  {
+background("red");
+
   }
 
+  
+
+  if (keyIsDown(LEFT_ARROW)) 
+  {
+    background("blue");
+    
+  }
+ 
+    if (keyIsDown(UP_ARROW)) 
+  {
+    background("yellow");
+   
+  }
+
+  if (keyIsDown(DOWN_ARROW)) 
+  {
+    background("green");
+  }
+
+
+  
   drawSprites();
 }
+
