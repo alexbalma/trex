017e3e07bb8be91422d86c8ff656041034abb5c9
alexbalma <99622713+alexbalma@users.noreply.github.com>
2022-02-16 13:48:28 -0600

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,44 +1,47 @@
+var sea,ship;
+var seaImg,shipImg;
 
-function setup() {
-  createCanvas(400,400);
-  background(51);
-  box = createSprite(200,200,30,30);
+function preload(){
+  //descomentar el código para agregar la animación del barco 
 
+  shipImg1 = loadAnimation("ship-1.png");
+  //shipImg1 = loadAnimation("ship-1.png");
+  //shipImg1 = loadAnimation("ship-1");
+  shipImg1 = loadAnimation("ship-1.png","ship-2.png","ship-1.png","ship-2.png");
+  //shipImg1 = loadAnimation("ship-1","ship-2","ship-1","ship-2");
+  
+  seaImg = loadImage("sea.png");
 }
 
-function draw() 
-{
+function setup(){
+  createCanvas(400,400);
+  background("blue");
 
-  // escribir el código para cambiar el color del fondo \
-  background("yellow");
-  // rojo cuando se presiona RIGHT_ARROW (tecla de flecha derecha)
-  if (keyIsDown(RIGHT_ARROW)) 
-  {
-background("red");
-
-  }
+  // Mover el fondo
+  sea=createSprite(400,200);
+  sea.addImage(seaImg);
+  sea.velocityX = -5;
+  sea.scale=0.3;
 
   
+  ship = createSprite(130,200,30,30);
+  ship.addAnimation("movingShip",shipImg1);
+  ship.scale =0.25;
+  
+}
 
-  if (keyIsDown(LEFT_ARROW)) 
-  {
-    background("blue");
-    
+function draw() {
+  background(0);
+  sea.velocityX = -3;
+
+  //descomentar el código para reiniciar el fondo
+  if(sea.x < 0){
+    //sea.x = 0;
+    //sea.x = sea.width;
+    sea.x = sea.width/8;
+    //sea.y = height;
   }
+
  
-    if (keyIsDown(UP_ARROW)) 
-  {
-    background("yellow");
-   
-  }
-
-  if (keyIsDown(DOWN_ARROW)) 
-  {
-    background("green");
-  }
-
-
-  
   drawSprites();
 }
-
